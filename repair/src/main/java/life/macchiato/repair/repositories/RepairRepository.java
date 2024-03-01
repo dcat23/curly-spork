8820fb3ecbfb4ab5c8f9e02c318dc0a4b27be380
Devin Catuns <devincatuns1@gmail.com>
2024-03-01 11:46:24 -0500

    add: eureka discovery client

--- repair/src/main/java/life/macchiato/repair/repositories/RepairRepository.java
+++
@@ -1,7 +0,0 @@
-package life.macchiato.repair.repositories;
-
-import life.macchiato.repair.models.Repair;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RepairRepository extends JpaRepository<Repair, Long> {
-}
