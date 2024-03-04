f267deae557181384c1c2e0be76fa605256a5533
Devin Catuns <devincatuns1@gmail.com>
2024-03-03 20:33:10 -0500

    init ytdlp

---
+++ ytdlp/src/main/java/life/macchiato/ytdlp/YtdlpApplication.java
@@ -0,0 +1,13 @@
+package life.macchiato.ytdlp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtdlpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YtdlpApplication.class, args);
+    }
+
+}
---
+++ ytdlp/src/test/java/life/macchiato/ytdlp/YtdlpApplicationTests.java
@@ -0,0 +1,13 @@
+package life.macchiato.ytdlp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YtdlpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
